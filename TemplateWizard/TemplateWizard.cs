3e89337ee9610d57230232d8360cbbf11d542729
Dan Mohl <dmohl@yahoo.com>
2011-02-20 08:50:34 -0600

    Added a template wizard

---
+++ TemplateWizard/TemplateWizard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using VSLangProj;
+
+namespace TemplateWizard
+{
+    public class TemplateWizard : IWizard
+    {
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            var currentProject = (VSProject)project.Object;
+            if (project.Name == "App")
+            {
+                var mainReference = ((VSProject)currentProject.DTE.Solution.Item(0));
+                mainReference.References.AddProject(project);
+            }
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+    }
+}
