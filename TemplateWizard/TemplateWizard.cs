5bb6bd58bfb0a9ee23c5acda0f480386a8efe442
Dan Mohl <dmohl@yahoo.com>
2011-02-20 10:32:54 -0600

    Fixed the TemplateWizard implementation.

--- TemplateWizard/TemplateWizard.cs
+++ TemplateWizard/TemplateWizard.cs
@@ -1,26 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using EnvDTE;
 using Microsoft.VisualStudio.TemplateWizard;
 using VSLangProj;
+using System.Diagnostics;
 
 namespace TemplateWizard
 {
     public class TemplateWizard : IWizard
     {
+        protected DTE Dte { get; set; }
         public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
+            Dte = (DTE) automationObject;
         }
 
         public void ProjectFinishedGenerating(Project project)
         {
-            var currentProject = (VSProject)project.Object;
-            if (project.Name == "App")
+            try
             {
-                var mainReference = ((VSProject)currentProject.DTE.Solution.Item(0));
-                mainReference.References.AddProject(project);
+                Project appProject = null;
+                Project appHostProject = null;
+                for (int i = 1; i <= Dte.Solution.Projects.Count; i++)
+                {
+                    switch (Dte.Solution.Projects.Item(i).Name)
+                    {
+                        case "App":
+                            appProject = Dte.Solution.Projects.Item(i);
+                            break;
+                        case "AppHost":
+                            appHostProject = Dte.Solution.Projects.Item(i);
+                            break;
+                    }
+                }
+
+                if (appHostProject != null && appProject != null)
+                {
+                    ((VSProject)appHostProject.Object).References.AddProject(appProject);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(string.Format("Error: {0}\r\nStack Trace: {1}", ex.Message, ex.StackTrace));
             }
         }
-
         public void ProjectItemFinishedGenerating(ProjectItem projectItem)
         {
         }
